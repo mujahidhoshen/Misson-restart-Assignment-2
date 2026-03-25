d77eee32a2189a7e1e808d9ef777cae9f0cf2540
mujahidhoshen <mujahidhoshen73@gmail.com>
2026-03-25 23:24:07 +0300

    New component added

---
+++ src/components/RseolveIssue.jsx
@@ -0,0 +1,11 @@
+
+
+const RseolveIssue = ({resolveCard}) => {
+    return (
+        <div className="bg-green-200 rounded-xl p-5 my-3">
+            <h2 className="text-green-600 text-lg">{resolveCard.title}</h2>
+        </div>
+    );
+};
+
+export default RseolveIssue;
